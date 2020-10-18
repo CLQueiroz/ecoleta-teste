cd15b1f1d05d0a9c73c1d7b3677a4cde30753045
Caique <caiqueonisto@live.com>
2020-10-18 12:59:23 -0300

    Corrigido estrutura e caminho dos diretorios

---
+++ README.md
@@ -0,0 +1,52 @@
+![](https://imgur.com/KcQUQqp.png)
+
+NLW Rocketseat
+-------------
+
+Semana de muito aprendizado e network promovidos pela Rocketseat, com a produção de um marketplace de coleta de resíduos: **Ecoleta** - em homenagem à semana do meio ambiente.
+Código limpo e funcional utilizando refatoração; aplicado tanto no *Front-end como no back-end* , de forma responsiva (telas de diversos tamanhos) e com acessibilidade (para necessidades especiais).
+Aplicação dinâmica, roda a partir de um servidor e utiliza banco de dados: uma ampla base de um *Full Stack Developer*.
+
+-------------
+**Pré-requisitos**
+
+- [Node.js](https://nodejs.org/en/)
+- [Express](https://expressjs.com/pt-br/starter/installing.html)
+- [Nunjucks](https://www.sqlite.org/index.html)
+- [SQLite](https://mozilla.github.io/nunjucks/)
+
+
+ *```npm start ```*
+
+-------------
+
+####API de localidade do IBGE:
+
+`<estados>` : <https://servicodados.ibge.gov.br/api/v1/localidades/estados>
+
+`<municipios>` : <https://servicodados.ibge.gov.br/api/v1/localidades/distritos>
+
+Para melhor utilização, instalado o plugin [JSON Formatter](https://chrome.google.com/webstore/detail/json-formatter/bcjindcccaagfpapjjmafapmmgkkhgoa?hl=pt-BR "JSON Formatter")
+
+-------------
+
+**Aplicabilidade**
++ Programação
+    + Algoritmos
+    + Lógica de Programação
+    + Linguagem: Javascript
++  Tecnologias no Front-end
+    + HTML
+    + CSS
+    + Javascript
++  Tecnologias no Back-end
+    * Javascript
+    * Node.js
+    * Banco de dados: SQL
+	 * SQLite
+
+![](https://i.imgur.com/D275Ge5.png)
+
+------------
+
+Aprender sempre. Evoluir constantemente 🚀
---
+++ src/server.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const server = express();
+
+const db = require("./database/db");
+
+server.use(express.static('public'));
+
+
+server.use(express.urlencoded({extended: true})); // habilita uso do req.body
+
+
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+});
+
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "Um título"});
+});
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html");
+});
+
+server.post("/savepoint", (req, res) => {
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items        
+    ]
+
+    function afterInsertData(err) {
+        if(err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+        return res.render("create-point.html", {saved: true})
+    }
+
+    db.run(query, values, afterInsertData) // incluir dados    
+});
+
+
+server.get("/search", (req, res) => {
+    const search = req.query.search
+    if(search == "") {
+        return res.render("search-results.html", { total: 0});
+    };
+    
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) { //puxando os dados do banco de dados
+        if(err) {
+            return console.log(err)
+        }
+        const total = rows.length
+        return res.render("search-results.html", { places: rows, total: total});
+    });
+});
+
+server.listen(3333); //configura porta 3000 como sendo responsável para execução do servidor
+
+// rodar o servidor: node start
+// gerar/atualizar o banco de dados: node src/database/db.js
